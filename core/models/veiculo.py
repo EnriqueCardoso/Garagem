df6a2e0f19d74826c078aef862cda0ead2edc793
Enrique Cardoso <131266631+EnriqueCardoso@users.noreply.github.com>
2025-04-29 16:36:07 -0300

    Atualização em Veículo

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -3,8 +3,8 @@ from django.db import models
 class Veiculo(models.Model):
     ano = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=0)
-    modelo = models.ForeignKey('Modelo', on_delete=models.CASCADE)
-    cor = models.ForeignKey('Cor', on_delete=models.CASCADE)
+    modelo = models.ForeignKey('Modelo', on_delete=models.PROTECT)
+    cor = models.ForeignKey('Cor', on_delete=models.PROTECT)
     acessorios = models.ManyToManyField('Acessorios', blank=True)
 
     def __str__(self):
